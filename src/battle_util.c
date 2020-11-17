90acbafddc2a0d1ca1f46aa9b0ced12ff8b8a28a
J3S5E <67044022+J3S5E@users.noreply.github.com>
2020-11-17 12:37:02 +0800

    Updated Crit multiplier and crit percentage

--- src/battle_util.c
+++ src/battle_util.c
@@ -6510,7 +6510,7 @@ static u32 CalcFinalDmg(u32 dmg, u16 move, u8 battlerAtk, u8 battlerDef, u8 move
 
     // check crit
     if (isCrit)
-        dmg = ApplyModifier((B_CRIT_MULTIPLIER >= GEN_6 ? UQ_4_12(1.5) : UQ_4_12(2.0)), dmg);
+        dmg = ApplyModifier(UQ_4_12(2.0), dmg);
 
     // check burn
     if (gBattleMons[battlerAtk].status1 & STATUS1_BURN && IS_MOVE_PHYSICAL(move)
